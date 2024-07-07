b62cdca2ac9deb92e3b08a733e1a59762111dd7b
ezequielmz <93398008+ezequielmz@users.noreply.github.com>
2024-07-07 15:49:53 -0300

    first commit

---
+++ src/test/java/com/AluraChallenge/ForoHub/ForoHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AluraChallenge.ForoHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
